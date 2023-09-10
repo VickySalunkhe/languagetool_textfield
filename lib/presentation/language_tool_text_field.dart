93284d65e2551cf4b12ea97dd32bfa5461dcd92a
Vicky Salunkhe <salunkhevicky01@gmail.com>
2023-09-10 23:50:20 +0530

    Update language_tool_text_field.dart
    Added new fields to default TextField widget

--- lib/presentation/language_tool_text_field.dart
+++ lib/presentation/language_tool_text_field.dart
@@ -6,6 +6,18 @@ import 'package:languagetool_textfield/utils/popup_overlay_renderer.dart';
 /// A TextField widget that checks the grammar using the given
 /// [LanguageToolController]
 class LanguageToolTextField extends StatefulWidget {
+  /// A Build Counter for text limit 
+  final Widget? Function(BuildContext, {required int currentLength, required bool isFocused, required int? maxLength})? buildCounter;
+
+  /// Called when the user initiates a change to the TextField's value: when they have inserted or deleted text.
+  final Function(String)? onChanged;
+    
+  /// TextInput Type for keyboard type
+  final TextInputAction? textInputAction;
+
+  /// Text Align for Text Field
+  final TextAlign? textAlign;
+  
   /// A style to use for the text being edited.
   final TextStyle? style;
 
@@ -41,6 +53,10 @@ class LanguageToolTextField extends StatefulWidget {
     this.mistakePopup,
     this.maxLines = 1,
     this.minLines,
+    this.buildCounter,
+    this.textInputAction,
+    this.onChanged,
+    this.textAlign = TextAlign.start,
     this.expands = false,
     super.key,
   });
@@ -92,9 +108,13 @@ class _LanguageToolTextFieldState extends State<LanguageToolTextField> {
           padding: const EdgeInsets.all(_padding),
           child: Center(
             child: TextField(
+              buildCounter: widget.buildCounter,
               focusNode: _focusNode,
               controller: widget.controller,
+              onChanged: widget.onChanged,
               scrollController: _scrollController,
+              textInputAction: widget.textInputAction,
+              textAlign: widget.textAlign,
               decoration: inputDecoration,
               minLines: widget.minLines,
               maxLines: widget.maxLines,
