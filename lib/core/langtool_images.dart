3d2f2a0c241a020622a2030450d1ef107bce1300
Vicky Salunkhe <salunkhevicky01@gmail.com>
2023-09-20 17:16:05 +0530

    Update langtool_images.dart

--- lib/core/langtool_images.dart
+++ lib/core/langtool_images.dart
@@ -2,6 +2,7 @@
 class LangToolImages {
   /// LanguageTool logo
   static const logo = 'images/symbol_boxed_white.png';
-
+  static const grammarLogo = 'images/grammar.png';
+  
   LangToolImages._();
 }
